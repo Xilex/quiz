3cd6f234fb2888534efbc42295d6de1e5b85453a
Xilex <jmramos80@hotmail.com>
2015-08-03 22:04:48 +0200

    Ej 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,21 +1,105 @@
 var models = require ('../models/models.js');
 
-//GET quizes
-exports.index = function (req, res){
-	models.Quiz.findAll().success(function(quiz){
-		res.render('quizes/question', { pregunta: quiz[0].pregunta});
-		})
+//Autoload - factoriza el codigo si ruta incluye :quizId
+exports.load = function(req, res, next, quizId){
+	models
+	.Quiz.find({
+		where: {id: Number(quizId)},
+		include: [{model: models.Comment}]
+	})
+	.then(
+		function(quiz){
+			if (quiz){
+				req.quiz = quiz;
+				next();
+			}
+			else {
+				next(new Error('No existe quizId='+quizId));
+			}
+		}).catch(function (error){next(error);});
 };
 
-//GET quizes/answer
+//GET quizes
+exports.index = function (req, res){
+	var search="%";
+	if(req.query.search && req.query.search.trim().length){
+		search=req.query.search.replace(/\s/g,"%");
+		search="%"+search+"%";
+		search=search.toLowerCase();
+	}
+	models
+	.Quiz.findAll({where: ["LOWER(pregunta) like ?", search],order:"pregunta"})
+	.then(function(quizes){
+		res.render('quizes/index.ejs', {quizes: quizes, search: req.query.search, errors: []});	
+	}).catch(function (error){next(error);});
+};
+
+//GET quizes/:id
+exports.show = function (req, res){
+	res.render('quizes/show', {quiz: req.quiz, errors: []});	
+};
+
+//GET quizes/:id/answer
 exports.answer = function(req, res) {
-	models.Quiz.findAll().success(function(quiz){
-		if (req.query.respuesta === quiz[0]respuesta){
-			res.render('quizes/answer', { respuesta: 'Correcto' }); 
-	    } else {
-			res.render('quizes/answer', { respuesta: 'Incorrecto' });
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta.toLowerCase()== req.quiz.respuesta.toLowerCase()){
+	        resultado = 'Correcto';
+	    }
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
+};
+
+//GET quizes/new
+exports.new =  function (req, res) {
+	var quiz = models.Quiz.build({pregunta: "Pregunta", respuesta: "Respuesta"});
+	res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+//POST quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+  	quiz
+  	.validate()
+  	.then(
+    	function(err){
+			if (err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+				quiz 
+				.save({fields: ["pregunta","respuesta","tema"]})
+				.then( function(){ res.redirect('/quizes')}) 
+    		}
+  		});
+	};
+
+
+//GET /quizes/:quizId/edit
+exports.edit = function (req,res){
+	var quiz = req.quiz; //autoload de instancia de quiz
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+//PUT /quizes/:quizId  
+exports.update = function (req,res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema = req.body.quiz.tema;
+	req.quiz
+	.validate()
+	.then(function(err){
+		if(err){
+			res.render('quizes/edit', {quiz:req.quiz, errors: err.errors});
+		} else {
+			req.quiz
+			.save({fields: ["pregunta","respuesta","tema"]})
+			.then(function (){res.redirect('/quizes')});
 		}
 	})
 };
 
-
+//DELETE /quizes/:quizId  
+exports.destroy = function (req,res){
+	req.quiz.destroy()
+	.then(function(){
+		res.redirect('/quizes');})
+	.catch(function(error){next(error)});
+};
